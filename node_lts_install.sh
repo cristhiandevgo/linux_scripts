8b5784475758c460dcf4882207792585554f92aa
cristhiandevgo <ivancristhian@hotmail.com>
2023-02-04 21:51:54 -0300

    Update Node (v18.14.0 - LTS)

--- node_lts_install.sh
+++ node_lts_install.sh
@@ -1,13 +1,13 @@
-# Nodejs - LTS - v16.16.0
+# Nodejs - LTS - v18.14.0
 cp "$HOME/.bashrc" "$HOME/.bashrc_backup_$(date)"
 cp "$HOME/.profile" "$HOME/.profile_backup_$(date)"
 mkdir $HOME/.node
-curl https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz --output node-v16.16.0-linux-x64.tar.xz
-tar -xf node-v16.16.0-linux-x64.tar.xz -C $HOME/.node/
+curl https://nodejs.org/dist/v18.14.0/node-v18.14.0-linux-x64.tar.xz --output node-v18.14.0-linux-x64.tar.xz
+tar -xf node-v18.14.0-linux-x64.tar.xz -C $HOME/.node/
 
 echo -n '
 # Node
-export NODEJS_HOME=$HOME/.node/node-v16.16.0-linux-x64/bin
+export NODEJS_HOME=$HOME/.node/node-v18.14.0-linux-x64/bin
 export PATH=$NODEJS_HOME:$PATH
 ' | tee -a ~/.profile ~/.bashrc
 
